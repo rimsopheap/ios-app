3f3182cabb156421511d47fcc640a93cfd0080a6
Rim Sopheap <rimsopheap@gmail.com>
2024-05-04 23:46:42 +0700

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Sopheap on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
